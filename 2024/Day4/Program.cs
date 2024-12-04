57e0af89d286bb4a132e7e41c60ad49d48e274d8
Kris Gersen <kriskes07@gmail.com>
2024-12-04 16:56:38 +0100

    Day4 part2

---
+++ 2024/Day4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"Part 2: {Part2()}");
+
+int Part2()
+{
+    var input = File.ReadLines($"{Environment.CurrentDirectory}/input.txt").Select(l => l.ToCharArray()).ToArray();
+    var ans = 0;
+    for (var x = 0; x < input.Length; x++)
+    for (var y = 0; y < input[1].Length; y++)
+        if (input[x][y] == 'A' && x - 1 >= 0 && y - 1 >= 0 && x + 1 < input.Length && y + 1 < input.Length)
+            if (Math.Abs(input[x + 1][y + 1] - input[x - 1][y - 1]) is 6 && Math.Abs(input[x - 1][y + 1] - input[x + 1][y - 1]) is 6)
+                ans++;
+    return ans;
+}
